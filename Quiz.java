706dd2069bd3b74a655b8300ffdb4f9c230bcc43
Kevin Canez <jkcanez@asu.edu>
2011-04-22 04:39:18 -0700

    I fucked your mother

--- Quiz.java
+++ Quiz.java
@@ -2,16 +2,30 @@ import java.util.*;
 
 public class Quiz
 {
-	private String title;
-	private Vector<Question> question;
 
-	public Quiz(String _title, Vector<Question> _question)
+	private int quizId;
+	private int questionNumber;
+	private String question;
+	private String answer1;
+	private String answer2;
+	private String answer3;
+	private String answer4;
+	private String correctAnswer;
+
+	public Quiz(int temp_quizId, int temp_questionNumber, String temp_question, String temp_ans1, String temp_ans2,
+					String temp_ans3, String temp_ans4, String temp_correctAnswer)
 	{
-		title = _title;
-		question = _question;
+		quizId = temp_quizId;
+		questionNumber = temp_questionNumber;
+		question = temp_question;
+		answer1 = temp_ans1;
+		answer2 = temp_ans2;
+		answer3 = temp_ans3;
+		answer4 = temp_ans4;
+		correctAnswer = temp_correctAnswer;
 	}
 
-	public int grade(Vector<String> a)
+	/*public int grade(Vector<String> a)
 	{
 		int sum = 0;
 		Question tempQ;
@@ -30,15 +44,28 @@ public class Quiz
 	public Vector<Question> getQuestion()
 	{
 		return question;
-	}
-	
-	public String getTitle()
+	}*/
+
+	public int getId()
 	{
-		return title;
+		return quizId;
+	}
+
+	public int getQuestionNumber()
+	{
+		return questionNumber;
 	}
 
 	public String toString()
 	{
-		return title + "\t" + question.size() + " questions";
+		return questionNumber + ":\t" + question + "\n\t" + answer1 + "\n\t" + answer2 + "\n\t" + answer3 + "\n\t" + answer4;
+	}
+
+	public boolean checkAnswer(String userAnswer)
+	{
+		if(correctAnswer.equals(userAnswer))
+			return true;
+		else
+			return false;
 	}
 }
